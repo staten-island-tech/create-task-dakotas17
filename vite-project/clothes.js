40b391af46b380da8d4a58630eb0de9562510203
dakotas17 <dakotas17@nycstudents.net>
2024-01-17 09:24:06 -0500

    kgaduj

---
+++ vite-project/clothes.js
@@ -0,0 +1,49 @@
+const clothes = [
+  { 
+    type: "hat",
+    img: "https://www.vintageyearstore.com/cdn/shop/files/163-1183-003-all_003_frontview_large_1.jpg?v=1703240929",
+  },{ 
+    type: "hat",
+    img: "https://m.media-amazon.com/images/I/71GRrhLfSgL._AC_UF1000,1000_QL80_.jpg",
+  },{ 
+    type: "hat",
+    img: "https://target.scene7.com/is/image/Target/GUEST_a4f86617-9f53-4552-ba0b-825e64221fb0?wid=488&hei=488&fmt=pjpeg",
+},{ 
+    type: "",
+    img: "",
+ },{
+    type: "",
+    img: "",
+ },{ 
+    type: "",
+    img: "",
+  },{ 
+    type: "bottoms",
+    img: "https://www.lochcarron.co.uk/media/catalog/product/cache/3c36a3980388d2eb9643903829ced6cc/y/s/yskr-str-m-stewart-royal-modern-classic-kilted-skirt-front-resized-2022-72dpi-rgb.jpg",
+  },{ 
+    type: "bottoms",
+    img: "https://www.maison-alaia.com/49/49864726RV_999_f.jpg",
+},{ 
+    type: "bottoms",
+    img: "",
+ },{
+    type: "",
+    img: "",
+ },{ 
+    type: "",
+    img: "",
+  },{ 
+    type: "",
+    img: "",
+  },{ 
+    type: "",
+    img: "",
+},{ 
+    type: "",
+    img: "",
+ },{
+    type: "",
+    img: "",
+ }
+];
+export { clothes };
