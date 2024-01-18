92d7bf27e6ed4860ca663de9a10df024c3f72377
dakotas17 <dakotas17@nycstudents.net>
2024-01-18 09:24:31 -0500

    awdaoih

--- vite-project/clothes.js
+++ vite-project/clothes.js
@@ -9,14 +9,14 @@ const clothes = [
     type: "hat",
     img: "https://target.scene7.com/is/image/Target/GUEST_a4f86617-9f53-4552-ba0b-825e64221fb0?wid=488&hei=488&fmt=pjpeg",
 },{ 
-    type: "",
-    img: "",
+    type: "top`",
+    img: "https://images.squarespace-cdn.com/content/v1/6212f172abb4ff0f9c29be5e/1654796730789-RUQW4OV6MO6OJO3IEVL9/unisex-staple-t-shirt-navy-front-62a22f8dd1355.png?format=1000w",
  },{
-    type: "",
-    img: "",
+    type: "top",
+    img: "https://ih1.redbubble.net/image.4786684630.5655/pp,504x498-pad,600x600,f8f8f8.u5.jpg",
  },{ 
-    type: "",
-    img: "",
+    type: "top",
+    img: "https://i.pinimg.com/originals/e6/ed/aa/e6edaaa7a95ab1e46d2346cc685ad866.png",
   },{ 
     type: "bottoms",
     img: "https://www.lochcarron.co.uk/media/catalog/product/cache/3c36a3980388d2eb9643903829ced6cc/y/s/yskr-str-m-stewart-royal-modern-classic-kilted-skirt-front-resized-2022-72dpi-rgb.jpg",
@@ -25,25 +25,16 @@ const clothes = [
     img: "https://www.maison-alaia.com/49/49864726RV_999_f.jpg",
 },{ 
     type: "bottoms",
-    img: "",
+    img: "https://images.squarespace-cdn.com/content/v1/6136ee30d6ef722b639dbca2/1639554083351-MNBFIHTY787WLH0NHOTO/Template+41_ccexpress.png?format=1500w",
  },{
-    type: "",
-    img: "",
+    type: "feet",
+    img: "https://www.hotsox.com/cdn/shop/files/HSW10271_PINK_FLAT_850x.png?v=1698699107",
  },{ 
-    type: "",
-    img: "",
+    type: "feet",
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Image_of_a_pair_of_Tmerberland_boots.png/800px-Image_of_a_pair_of_Tmerberland_boots.png",
   },{ 
-    type: "",
-    img: "",
-  },{ 
-    type: "",
-    img: "",
-},{ 
-    type: "",
-    img: "",
- },{
-    type: "",
-    img: "",
- }
+    type: "feet",
+    img: "https://ekicks.eu/cdn/shop/files/UGGtazzslippersmustard2_2048x2048.png?v=1697289467",
+  }
 ];
 export { clothes };
