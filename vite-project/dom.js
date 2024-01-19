83aee1371a3cb367d648904c2cfe9a8557ae767f
dakotas17 <dakotas17@nycstudents.net>
2024-01-19 09:24:32 -0500

    wada

---
+++ vite-project/dom.js
@@ -0,0 +1,8 @@
+const DOMSelectors = {
+    container: document.querySelector(".container"),
+    cards: document.querySelectorAll(".cards"),
+   buttons: document.querySelectorAll(".buttons"),
+   form: document.getElementById("filters")
+};
+
+export {DOMSelectors};
--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,18 @@
+import { clothes } from './clothes';
 import './style.css'
+import { DOMSelectors } from './dom'
+
+function add(arr) {
+    arr.forEach((bruh) => {
+      DOMSelectors.container.insertAdjacentHTML("afterbegin",
+      `
+      <div class="card">
+      <img src="${clothes[].img}" alt="" class="card-img">
+      </div>
+      `,
+      )
+    });
+  }
+
+  add(clothes);
+  console.log(clothes)
