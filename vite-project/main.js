a4cf6eba99fa59e796a195eba24bf15d122df516
dakotas17 <dakotas17@nycstudents.net>
2024-01-22 09:24:38 -0500

    rsesr

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,18 +1,79 @@
-import { clothes } from './clothes';
+import { tops } from './clothes';
+import { hats } from './clothes';
+import { bottoms } from './clothes';
+import { feet } from './clothes';
 import './style.css'
 import { DOMSelectors } from './dom'
 
-function add(arr) {
-    arr.forEach((bruh) => {
-      DOMSelectors.container.insertAdjacentHTML("afterbegin",
+  function addf(arr) {
+    document.querySelector(".container").insertAdjacentHTML("afterbegin",
       `
       <div class="card">
-      <img src="${clothes[].img}" alt="" class="card-img">
+      <img src="${feet[0].img}" alt="" class="card-img">
       </div>
       `,
       )
-    });
-  }
+    };
+  
 
-  add(clothes);
-  console.log(clothes)
+  addf(feet);
+  console.log(feet)
+
+
+  function addb(arr) {
+    document.querySelector(".container").insertAdjacentHTML("afterbegin",
+      `
+      <div class="card">
+      <img src="${bottoms[0].img}" alt="" class="card-img">
+      </div>
+      `,
+      )
+    };
+  
+
+  addb(bottoms);
+  console.log(bottoms)
+
+  function addt(arr) {
+    document.querySelector(".container").insertAdjacentHTML("afterbegin",
+      `
+      <div class="card">
+      <img src="${tops[0].img}" alt="" class="card-img">
+      </div>
+      `,
+      )
+    };
+  
+
+  addt(tops);
+  console.log(tops)
+  
+function addh(arr) {
+  document.querySelector(".container").insertAdjacentHTML("afterbegin",
+    `
+    <div class="card">
+    <img src="${hats[0].img}" alt="" class="card-img">
+    </div>
+    `,
+    )
+  };
+
+
+addh(hats);
+console.log(hats)
+
+/* 
+function addh(arr) {
+  arr.forEach((bruh) => {
+    DOMSelectors.container.insertAdjacentHTML("afterbegin",
+    `
+    <div class="card">
+    <img src="${hats[0].img}" alt="" class="card-img">
+    </div>
+    `,
+    )
+  });
+}
+
+addh(hats);
+console.log(hats) */
